5c3bbcc86ddc5a6a586f2a01158aeca42e8800e3
Youngho Cho <dominoyh5@gmail.com>
2020-08-29 18:08:28 +0900

    fix: weather_post.ino http get

--- arduino/weather_post/weather_post.ino
+++ arduino/weather_post/weather_post.ino
@@ -1,53 +1,47 @@
+// http
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+// sensors
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
 #include "DHT.h"
 
 DHT dht(14, DHT22);
 Adafruit_BMP085 bmp;
-  
+
 void setup() {
+  // serial
   Serial.begin(9600);
+
+  // wifi
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("seryu-elec", "cho1418.");
+
+  // dht, bmp
   dht.begin();
   bmp.begin();
 }
-  
+
 void loop() {
-  
-  float dht22_Humi = dht.readHumidity();
-  float dht22_Temp = dht.readTemperature();
-  float bmp180_Temp = bmp.readTemperature();
-  float bmp180_Pres = bmp.readPressure();
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+    WiFiClient client;
+    HTTPClient http;
 
-  Serial.print("DHT22 Temp : ");
-  Serial.print(dht22_Temp);
-  Serial.println(" *C");
-
-  Serial.print("DHT22 Humi : ");
-  Serial.print(dht22_Humi);
-  Serial.println(" %");
-  
-  Serial.print("BMP180 Temp : ");
-  Serial.print(bmp180_Temp);
-  Serial.println(" *C");
-  
-  Serial.print("BMP180 Pressure : ");
-  Serial.print(bmp.readPressure());
-  Serial.println(" hPa");
-//    
-//    Serial.print("Altitude = ");
-//    Serial.print(bmp.readAltitude());
-//    Serial.println(" meters");
-//
-//    Serial.print("Pressure at sealevel (calculated) = ");
-//    Serial.print(bmp.readSealevelPressure());
-//    Serial.println(" Pa");
-//
-//    Serial.print("Real altitude = ");
-//    Serial.print(bmp.readAltitude(101500));
-//    Serial.println(" meters");
+    if (http.begin(client, "http://192.168.127.13:8080/headers")) {
+      int httpCode = http.GET();
+      Serial.printf("httpCode: %d\n", httpCode);
+      http.end();
+    } else {
+      Serial.printf("failed\n");
+    }
     
-    Serial.println();
-    Serial.println("----------------------------");
-    Serial.println();
-    delay(2000);
+  }
+
+  delay(2000);
 }
